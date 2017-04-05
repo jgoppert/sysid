6e351dca79970fe05c83a7be20bb87ea07d305a3
James Goppert <james.goppert@gmail.com>
2017-04-05 00:42:42 -0400

    Switch to versioneer.

--- sysid/__init__.py
+++ sysid/__init__.py
@@ -3,3 +3,7 @@ Default imports.
 """
 from .ss import StateSpaceDiscreteLinear, StateSpaceDataList, StateSpaceDataArray
 from .subspace import subspace_det_algo1, prbs, nrms
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
